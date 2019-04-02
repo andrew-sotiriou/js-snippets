5f054194dfd256413793f03137b31d50b8db3fd8
Andrew <drew.sotiriou@gmail.com>
2019-04-02 17:29:07 -0400

    Run build script, add foreach and every to index.html. Add ForEach JS to arrays.js.

--- src/js/arrays.js
+++ src/js/arrays.js
@@ -18,16 +18,27 @@ updatePreES6Element(onlyNumbersPre, "array-filter-container");
 updatePostES6Element(onlyNumbersPost, "array-filter-container");
 
 //.REDUCE
-//example 1
+//example 1:
 const nameArray = ['adam', 'nancy', 'damain', 'ramona', 'evan', 'wendy'];
 let newNamePre = nameArray.reduce((acc, currVal) => { return acc + currVal[0] }, "");
 newNamePre = newNamePre.charAt(0).toUpperCase() + newNamePre.slice(1);
 updatePreES6Element(newNamePre, "array-reduce-container");
 
 //example 2:
-// pre-es6-output.example2
 const reduceNums = [1, 2, 3, 4,];
 let reduceSumPre = reduceNums.reduce((acc, val) => { return acc + val; }, 0);
 let reduceSumPost = reduceNums.reduce((acc, val) => acc + val, 1);
 updatePreES6Element(reduceSumPre, "array-reduce-container", "pre-es6-output-example2");
-updatePreES6Element(reduceSumPost, "array-reduce-container","post-es6-output-example2");
+updatePostES6Element(reduceSumPost, "array-reduce-container","post-es6-output-example2");
+
+//FOREACH
+const foreachNumsPre = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+foreachNumsPre.forEach(function(num, i, array) {
+	if (num%2 == 0){
+		array.splice(i,1);
+	}
+});
+const foreachNumsPost = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+foreachNumsPost.forEach( (num, i, array) => {if (num%2 == 0) array.splice(i,1)});
+updatePreES6Element(foreachNumsPre, "array-foreach-container");
+updatePostES6Element(foreachNumsPost, "array-foreach-container");

8d84e77104d926ba211f565bc64176c386cd9691
Andrew <drew.sotiriou@gmail.com>
2019-04-02 14:43:12 -0400

    Move Accordion JS from index to newly accordion.js and import accordion into index.js

---
+++ src/js/accordion.js
@@ -0,0 +1,16 @@
+//Accordion
+document.addEventListener('click', function (event) {
+	//Do nothing if not clicking a section-header
+	if (!event.target.classList.contains('section-header')) return;
+
+	var block = event.target.nextSibling.nextSibling.classList;
+	if (block.contains('active')){
+		block.remove('active');
+    	return;
+	}
+	else {
+		var openAccordions = document.querySelectorAll('.section-container.active');
+		openAccordions.forEach(openAccordions => { openAccordions.classList.remove('active'); });
+		block.add('active');
+	}
+});

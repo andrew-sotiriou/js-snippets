67263c01760b5a113cac95205bdf9b09511ecca8
Andrew <drew.sotiriou@gmail.com>
2019-04-03 18:07:27 -0400

    Index.html and Arrays.js -> add sort method

--- src/js/arrays.js
+++ src/js/arrays.js
@@ -21,7 +21,7 @@ addValue(reduceValue, "array-reduce-container");
 //example 2:
 const reduceNums = [1, 2, 3, 4,];
 let reduceSum = reduceNums.reduce((acc, val) => acc + val, 1);
-addValue(reduceSum, "array-reduce-container","example2-output");
+addValue(reduceSum, "array-reduce-container","example-output2");
 
 //FOREACH
 let foreachNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -32,6 +32,17 @@ addValue(foreachNums, "array-foreach-container");
 const everyNums = [1, 50, 75, 200, 350, 525, 1000];
 let everyNumsValue = everyNums.every(num => num > 0);	
 addValue(everyNumsValue, "array-every-container");
+
+//SORT
+let sortNames = ["Stuart", "Mark", "Bob", "Dave", "Phil", "John", "Tim", "Jerry", "Carl", "Kevin",];
+sortNames.sort();
+addValue(sortNames, "array-sort-container");
+sortNames.reverse();
+addValue(sortNames, "array-sort-container", "example-output1-reversed");
+
+let sortNums = [23, 22, 4, 16, 8, 42, 15];
+sortNums.sort((a, b) => a - b);
+addValue(sortNums, "array-sort-container", "example-output2");
 
 //Check Arrays for Non-Dups
 const nondupsList1 = [1,2,3,4,5,6,7];

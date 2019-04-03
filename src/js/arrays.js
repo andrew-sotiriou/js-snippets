fd2dedad901a781369e80a9f72f98cbea2e7641a
Andrew <drew.sotiriou@gmail.com>
2019-04-03 16:36:40 -0400

    Index.html && arrays.js -> Removed all the code for pre-es6 and just showing code with newer concepts. WriteValues.js -> removed two of teh older write to output examples and created 1 new one, when importing to arrays.js used shortName in arrays.js ran build script

--- src/js/arrays.js
+++ src/js/arrays.js
@@ -1,44 +1,34 @@
-import { updatePreES6Element, updatePostES6Element } from './writeValues.js'
-
-//const arrayElementId = "array-map-container";
+import { addExampleValueToElement as addValue } from './writeValues.js'
 
 //Arrays
 //.MAP
-const numbers = [1, 2, 3, 4, 5];
-let squareNumbersPre = numbers.map(number => { return number * number; });
-let squareNumbersPost = numbers.map(number => number * number);
-updatePreES6Element(squareNumbersPre, "array-map-container");
-updatePostES6Element(squareNumbersPost, "array-map-container");
+const mapNumbers = [1, 2, 3, 4, 5];
+let mapValue = mapNumbers.map(number => number * number);
+addValue(mapValue, "array-map-container");
 
 //.FILTER
-const things = ['desk', 'chair', 5, 'backpack', 3.14, 100];
-let onlyNumbersPre = things.filter(thing => { return typeof thing === 'number'; });
-let onlyNumbersPost = things.filter(thing => typeof thing === 'number');
-updatePreES6Element(onlyNumbersPre, "array-filter-container");
-updatePostES6Element(onlyNumbersPost, "array-filter-container");
+const filterThings = ['desk', 'chair', 5, 'backpack', 3.14, 100];
+let filterOnlyNumbers = filterThings.filter(thing => typeof thing === 'number');
+addValue(filterOnlyNumbers, "array-filter-container");
 
 //.REDUCE
 //example 1:
-const nameArray = ['adam', 'nancy', 'damain', 'ramona', 'evan', 'wendy'];
-let newNamePre = nameArray.reduce((acc, currVal) => { return acc + currVal[0] }, "");
-newNamePre = newNamePre.charAt(0).toUpperCase() + newNamePre.slice(1);
-updatePreES6Element(newNamePre, "array-reduce-container");
+const reduceNameArray = ['adam', 'nancy', 'damain', 'ramona', 'evan', 'wendy'];
+let reduceValue = reduceNameArray.reduce((acc, currVal) => { return acc + currVal[0] }, "");
+reduceValue = reduceValue.charAt(0).toUpperCase() + reduceValue.slice(1);
+addValue(reduceValue, "array-reduce-container");
 
 //example 2:
 const reduceNums = [1, 2, 3, 4,];
-let reduceSumPre = reduceNums.reduce((acc, val) => { return acc + val; }, 0);
-let reduceSumPost = reduceNums.reduce((acc, val) => acc + val, 1);
-updatePreES6Element(reduceSumPre, "array-reduce-container", "pre-es6-output-example2");
-updatePostES6Element(reduceSumPost, "array-reduce-container","post-es6-output-example2");
+let reduceSum = reduceNums.reduce((acc, val) => acc + val, 1);
+addValue(reduceSum, "array-reduce-container","example2-output");
 
 //FOREACH
-const foreachNumsPre = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-foreachNumsPre.forEach(function(num, i, array) {
-	if (num%2 == 0){
-		array.splice(i,1);
-	}
-});
-const foreachNumsPost = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-foreachNumsPost.forEach( (num, i, array) => {if (num%2 == 0) array.splice(i,1)});
-updatePreES6Element(foreachNumsPre, "array-foreach-container");
-updatePostES6Element(foreachNumsPost, "array-foreach-container");
+let foreachNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+foreachNums.forEach( (num, i, array) => {if (num%2 == 0) array.splice(i,1)});
+addValue(foreachNums, "array-foreach-container");
+
+//EVERY
+const everyNums = [1, 50, 75, 200, 350, 525, 1000];
+let everyNumsValue = everyNums.every(num => num > 0);	
+addValue(everyNumsValue, "array-every-container");
--- src/js/writeValues.js
+++ src/js/writeValues.js
@@ -1,11 +1,6 @@
-function updatePreES6Element(value, elementID, elementClass = "pre-es6-output") {
-	var mapPre = document.getElementById(elementID).getElementsByClassName(elementClass)[0];
-	mapPre.innerHTML = value;
-}
-
-function updatePostES6Element(value, elementID, elementClass = "post-es6-output") {
+function addExampleValueToElement(value, elementID, elementClass = "example-output") {
 	var mapPost = document.getElementById(elementID).getElementsByClassName(elementClass)[0];
 	mapPost.innerHTML = value;
 }
 
-export {updatePreES6Element, updatePostES6Element};
+export {addExampleValueToElement};

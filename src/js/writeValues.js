e50c3b47feedda5f5f017351443e2fa72eb37090
Andrew <drew.sotiriou@gmail.com>
2019-04-04 11:27:23 -0400

    Updated addExampleValueToElement function, created processAJAXCalls function (this will be used for the other AJAX calls coming later), exported that call to be used in ajaxCalls.js, created function objectToString for default case, created function for proccess the data from Norris API (getNorrisJoke), created functions to create img and paragraph tag (createImg, createParagraph respectively)

--- src/js/writeValues.js
+++ src/js/writeValues.js
@@ -1,6 +1,48 @@
 function addExampleValueToElement(value, elementID, elementClass = "example-output") {
-	var mapPost = document.getElementById(elementID).getElementsByClassName(elementClass)[0];
-	mapPost.innerHTML = value;
+	let writeToElement = document.getElementById(elementID).getElementsByClassName(elementClass)[0];
+	writeToElement.innerHTML = value;
 }
 
-export {addExampleValueToElement};
+function processAJAXCalls(object, apiType, elementID, elementClass = "example-output") {	
+	let valueToWrite;
+	switch(apiType) {
+		case "norris":
+		valueToWrite = getNorrisJoke(object);
+		break;
+		default:
+		valueToWrite = objectToString(object);
+	};	
+	let writeValue = document.getElementById(elementID).getElementsByClassName(elementClass)[0];
+	writeValue.appendChild(valueToWrite);
+}
+
+function objectToString(object) {
+	let objectToString = JSON.stringify(object);
+	return objectToString;
+}
+
+function getNorrisJoke(object) {
+	let norrisImg = createImg(object.icon_url);
+	let norrisJoke = createParagraph(object.value);
+	norrisJoke.appendChild(norrisImg);
+	return norrisJoke;
+}
+
+function createImg(imgURL, imgAlt = null, imgTitle = null){
+	let imgTag = document.createElement("img");
+	imgTag.src = imgURL;
+	imgTag.setAttribute("class", "image");
+	imgTag.alt = imgAlt ? imgAlt : null;
+	imgTag.title = imgTitle ? imgTitle : null;
+	return imgTag; 
+}
+
+function createParagraph(text, url = null){
+	var	pTag = document.createElement("p");
+	pTag.setAttribute("class", "output-text");
+	var pText = document.createTextNode(text);
+	pTag.appendChild(pText);
+    return pTag;
+}
+
+export {addExampleValueToElement, processAJAXCalls};

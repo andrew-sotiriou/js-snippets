d53d06c8f6e89eb7bae05584c99b94e1dc31d27c
Andrew <drew.sotiriou@gmail.com>
2019-04-05 17:08:46 -0400

    HTML -> changed title AJAX to Network Calls, added Fetch Get API example, added Promises examples includes .ALL and .RACE. ajaxCalls.js -> changed the import call, updated calls due to import change, Added Fetch Get request. Ran build script.

--- src/js/ajaxCalls.js
+++ src/js/ajaxCalls.js
@@ -1,4 +1,4 @@
-import { processAJAXCalls as addValue } from './writeValues.js'
+import { addExampleValueToElement as addValue, processAJAXCalls as procCall } from './writeValues.js';
 
 //Original AJAX:
 const xhr = new XMLHttpRequest;
@@ -7,7 +7,7 @@ xhr.responseType = "json";
 xhr.onreadystatechange = function() {
 	if (xhr.readyState === XMLHttpRequest.DONE) {
 		if (xhr.status === 200) {
-			addValue(xhr.response, "norris", "ajax-original-container");
+			procCall(xhr.response, "norris", "ajax-original-container");
 		}
 		else {
 			return `Error ${xhr.status}`;
@@ -17,3 +17,31 @@ xhr.onreadystatechange = function() {
 
 xhr.open('GET', url, true);
 xhr.send();
+
+//Fetch GET
+let fetchURL = "http://taco-randomizer.herokuapp.com/random/";
+fetch(fetchURL)
+.then(response => {
+	return response.json();
+})
+.then(data => {
+	procCall(data, "taco", "ajax-fetch-container");
+})
+.catch(error => {
+	addValue(error, "ajax-fetch-container");
+});
+
+//Fetch POST
+/*let data = {
+    name: 'Sara'
+}
+// The parameters we are gonna pass to the fetch function
+let fetchData = { 
+    method: 'POST', 
+    body: data,
+    headers: new Headers()
+}
+fetch(url, fetchData)
+.then(function() {
+    // Handle response you get from the server
+});*/

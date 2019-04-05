87c0c81ccabc7e62f0202fc34723ddf4cb2d44c5
Andrew <drew.sotiriou@gmail.com>
2019-04-05 17:03:13 -0400

    Added file for examples of promises

---
+++ src/js/promises.js
@@ -0,0 +1,49 @@
+import { addExampleValueToElement as addValue } from './writeValues.js'
+
+//Simple Promise
+let simplePromise = new Promise((resolve, reject) => {
+	let num = Math.random();
+	if (num < .5 ){
+		resolve('This is a successful promise and makes us happy');
+	} 
+	else {
+		reject(new Error('This is an unsuccessful promise and makes us sad'));
+	}
+});
+
+simplePromise
+  .then((resolvedValue) => {
+    addValue(resolvedValue, "promises-simple-container");
+  })
+  .catch((rejectionReason) => {
+    addValue(rejectionReason, "promises-simple-container");
+  });
+
+//Promise.all
+
+//Successful
+Promise.all([
+  new Promise(resolve => setTimeout(() => resolve(1), 3000)),
+  new Promise(resolve => setTimeout(() => resolve(2), 2000)),
+  new Promise(resolve => setTimeout(() => resolve(3), 1000))
+])
+.then(values => addValue(values, "promises-all-container"))
+.catch(error => addValue(error, "promises-all-container")); 
+
+//UnSuccessful
+Promise.all([
+  new Promise((resolve, reject) => setTimeout(() => resolve(1), 1000)),
+  new Promise((resolve, reject) => setTimeout(() => reject(new Error("Whoops!")), 2000)),
+  new Promise((resolve, reject) => setTimeout(() => resolve(3), 3000))
+])
+.then(values => addValue(values, "promises-all-container", "example-output2"))
+.catch(error => addValue(error, "promises-all-container", "example-output2"));
+
+//Promise.race
+Promise.race([
+  new Promise(resolve => setTimeout(() => resolve(1), 3000)),
+  new Promise(resolve => setTimeout(() => resolve(2), 2000)),
+  new Promise(resolve => setTimeout(() => resolve(3), 1000))
+])
+.then(values => addValue(values, "promises-race-container"))
+.catch(error => addValue(error, "promises-race-container")); 

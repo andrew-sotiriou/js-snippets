c0acc6fda6ba15797a07af49dc3d08cce77b7cb4
Andrew <drew.sotiriou@gmail.com>
2019-04-09 11:24:42 -0400

    Add checkName and checkURL functions

--- src/js/otherFunctions.js
+++ src/js/otherFunctions.js
@@ -12,3 +12,27 @@ function getAge (bday){
   addValue(age, 'other-age-container');
 }
 getAge('01/01/1980');
+
+//Validate Name
+function checkName(name){  
+  var regex = /^[a-zA-x]{2,30}$/;
+  if( !((name != null && name != undefined) && regex.test(name)) ){
+  	addValue('Invalid Name', 'other-name-container');
+  }
+  else {
+  	addValue('Valid Name', 'other-name-container');
+  }
+}
+checkName('andrew');
+
+//URL Check
+function checkURL(url){
+	var regex = /^(ftp|http|https):\/\/[^ "]+$/;
+	if ( !((url != null && url != undefined) && regex.test(url)) ) {
+		addValue('Invalid URL', 'other-url-container');
+	}
+	else {
+		addValue('Valid URL', 'other-url-container');
+	} 
+}
+checkURL('http://www.google.com');

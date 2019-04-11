4bc419d7d2d21d49a92be06bf23a2bdcdfe7e2ae
Andrew <drew.sotiriou@gmail.com>
2019-04-11 11:35:48 -0400

    Import functions from writeValues.js and write the simple JQuery AJAX Get call to the countries API

--- src/js/jquery/JQajaxCalls.js
+++ src/js/jquery/JQajaxCalls.js
@@ -1,1 +1,14 @@
-console.log('I made it');
+import { addExampleValueToElement as addValue, processAJAXCalls as procCall } from '../writeValues.js';
+
+//JQUERY AJAX
+
+//GET CALL
+$.ajax({
+	url:"https://restcountries.eu/rest/v2/name/cyprus",
+	type: "GET",
+	dataType: 'json'
+}).done(function(data){
+	procCall(data, "country", "jquery-ajaxget-container");
+}).fail(function(error){
+	addValue(`${error.responseJSON.status} ${error.responseJSON.message}`, "jquery-ajaxget-container");
+});

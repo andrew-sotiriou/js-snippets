e978945e465be1c0e59fd758a812cdaee68becf1
Andrew <drew.sotiriou@gmail.com>
2019-05-07 10:12:28 -0400

    Updated the http API to fix issue on github pages and ran build script

--- src/js/ajaxCalls.js
+++ src/js/ajaxCalls.js
@@ -65,7 +65,7 @@ const fetchAsync = async (fetchAsynURL) => {
 	return data;
 };
 
-fetchAsync('http://numbersapi.com/random/trivia')
+fetchAsync('https://cors-anywhere.herokuapp.com/http://numbersapi.com/random/trivia')
 .then(data => {
 	procCall(data, "numbers", "ajax-asyncawait-container");
 })

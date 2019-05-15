12bf01496831d7156ce8321c14fe65d3aed3525b
Andrew <drew.sotiriou@gmail.com>
2019-05-15 02:09:07 -0400

    HTMLFive HTML page -> Include sections for HTML5 Drag and Drop and HTML5 History API.  htmlFive.js -> use this file to import all neccessary SCSS and JS files. htmlFive folder and htmlFive-api.js -> Add all JS for all the drag and drop events and appropriate event listeners, add history js calls and replacestate, pushstate, popstate and the code for working examples. Also include all these files for the first time

---
+++ src/js/htmlFive.js
@@ -0,0 +1,9 @@
+import '../scss/index.scss';
+
+import '../scss/htmlFive.scss';
+
+import michelangelo from "../images/michelangelo.png";
+
+import { accordion } from './accordion.js';
+
+import { htmlFive } from './htmlFive/htmlFive-api.js';
---
+++ src/js/htmlFive/htmlFive-api.js
@@ -0,0 +1,100 @@
+//Drag AND Drop
+const dragTurtleStart = (event) => {
+	event.dataTransfer.setData("text", event.target.id);
+	event.dataTransfer.dropEffect = 'move';
+	event.target.style.opacity = '.5';
+};
+
+const dragTurtleEnd = (event) => {
+	event.target.style.opacity = '';
+};
+
+const allowTurtleDrop = (event) =>  {
+	event.preventDefault();
+};
+
+const dropTurtle = (event) => {
+	event.target.style.background = '';
+	event.preventDefault();
+	var data = event.dataTransfer.getData("text");
+	event.target.appendChild(document.getElementById(data));
+};
+
+const dragBoxEnter = (event) => {
+	event.preventDefault();
+	event.dataTransfer.dropEffect = 'move';
+	event.target.style.background = 'orange';
+};
+
+const dragBoxLeave = (event) => {
+  event.target.style.background = '';
+};
+
+const draggableItem = document.getElementById("tmnt-michelangelo");
+const dropLocation1 = document.getElementById("dragbox1");
+const dropLocation2 = document.getElementById("dragbox2");
+
+draggableItem.addEventListener('dragstart', dragTurtleStart);
+draggableItem.addEventListener('dragend', dragTurtleEnd);
+dropLocation1.addEventListener('drop', dropTurtle);
+dropLocation2.addEventListener('drop', dropTurtle);
+dropLocation1.addEventListener('dragover', allowTurtleDrop);
+dropLocation2.addEventListener('dragover', allowTurtleDrop);
+dropLocation1.addEventListener('dragenter', dragBoxEnter);
+dropLocation2.addEventListener('dragenter', dragBoxEnter);
+dropLocation1.addEventListener('dragleave', dragBoxLeave);
+dropLocation2.addEventListener('dragleave', dragBoxLeave);
+
+//History API
+const historyBack = document.getElementById("historyBack");
+const historyForward = document.getElementById("historyForward");
+const historyGo = document.getElementById("historyGo");
+const historyRefresh = document.getElementById("historyRefresh");
+const historyLength = document.getElementById("historyLength");
+const historyReplaceState = document.getElementById("historyReplaceState");
+const historyPushState = document.getElementById("historyPushState");
+const historyReplacePushonPopState = document.getElementById("historyReplacePushonPopState");
+const popStateData = `There is unrest in the forest 
+There is trouble with the trees
+For the maples want more sunlight
+And the oaks ignore their pleas`;
+
+historyBack.addEventListener("click", function() {
+	window.history.back();
+});
+
+historyForward.addEventListener("click", function() {
+	window.history.forward();
+});
+
+historyGo.addEventListener("click", function() {
+	window.history.go(-2);
+});
+
+historyRefresh.addEventListener("click", function() {
+	window.history.go();
+});
+
+historyLength.addEventListener("click", function() {
+	alert(window.history.length);
+});
+
+historyReplaceState.addEventListener("click", function() {
+	history.replaceState(popStateData, "title", 'rush');
+});
+
+historyPushState.addEventListener("click", function() {
+	history.pushState(popStateData, "title", 'rush');
+});
+
+historyReplacePushonPopState.addEventListener("click", function() {
+	history.replaceState(popStateData, "title", 'trees');
+	history.pushState(popStateData, "title", 'trees');
+	window.history.back();
+});
+
+window.addEventListener('popstate', function(e) {
+	alert(popStateData);
+});
+
+//

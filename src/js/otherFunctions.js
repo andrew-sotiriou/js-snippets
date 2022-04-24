c41ebc99c6fb49ef20860871ea58417d63acaca6
Andrew Sotiriou <drew.sotiriou@gmail.com>
2022-04-23 23:21:51 -0400

    Add permutations script, add new indent to SCSS, and rebuild dist folder

--- src/js/otherFunctions.js
+++ src/js/otherFunctions.js
@@ -67,3 +67,19 @@ function getSize(multiSizes) {
 }
 
 addValue(getSize(multiSizes),"largest-size-container");
+
+const permutations = arr => {
+  if (arr.length <= 2) return arr.length === 2 ? [arr, [arr[1], arr[0]]] : arr;
+  return arr.reduce(
+    (acc, item, i) =>
+      acc.concat(
+        permutations([...arr.slice(0, i), ...arr.slice(i + 1)]).map(val => [
+          item,
+          ...val,
+        ])
+      ),
+    []
+  );
+ };
+ console.log(permutations(['Bill', 'Jim', 'bob', 'ash']));
+ addValue("Check the Console", "permutations-container");
